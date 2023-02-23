8b76d9e5cde7c7f25ebf3c8fe7a4461baabf3ad1
AdamJacobson3 <jacobsonadam10@gmail.com>
2023-02-23 15:32:19 +0000

    Gjorde om controller från scratch

--- src/se/lu/ics/Controller.java
+++ src/se/lu/ics/Controller.java
@@ -1,13 +1,5 @@
 package se.lu.ics;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.rmi.RemoteException;
-import javax.swing.JOptionPane;
-import se.lu.ics.WebApplicationSoapProxy;
-
 public class Controller {
-	private Application application;
-	private WebApplicationSoapProxy webService;
-	
+
 }
