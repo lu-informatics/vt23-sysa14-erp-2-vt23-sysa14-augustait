446d4c722d3b68dd277e38b909389b1b27477365
Nanna Amankwah <na6404am-s@student.lu.se>
2023-02-24 13:45:09 +0100

    "Fixat gränssnittet så att man kan köra och se"

--- src/se/lu/ics/Controller.java
+++ src/se/lu/ics/Controller.java
@@ -2,4 +2,25 @@ package se.lu.ics;
 
 public class Controller {
 
+	
+	private GUI gui;
+
+	public GUI getGui() {
+		return gui;
+	}
+
+	public void setGui(GUI gui) {
+		this.gui = gui;
+	}
+	
+	public Controller(GUI gui) {
+	this.gui = gui;
+	declareListener();
+}
+
+	public void declareListener() {
+	
+		
+}
+	
 }
